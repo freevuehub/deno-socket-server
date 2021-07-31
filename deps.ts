bf1b7514b204111c864931c772a6f4831e76349a
홍성준 <freevue@drx.gg>
2021-07-31 18:02:08 +0900

    [bugfix]: 버전 수정

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 import 'https://deno.land/x/dotenv/load.ts'
 
-export { WebSocketServer } from 'https://deno.land/x/websocket@v0.1.2/mod.ts'
+export { WebSocketServer } from 'https://deno.land/x/websocket@v0.1.1/mod.ts'
 
-export type { WebSocketClient } from 'https://deno.land/x/websocket@v0.1.2/mod.ts'
+export type { WebSocketClient } from 'https://deno.land/x/websocket@v0.1.1/mod.ts'
