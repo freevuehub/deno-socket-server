4b808f8acc4d4b5df4d0de9637ce545754c0ee62
freevuehub <hackish113@gmail.com>
2021-08-08 18:43:01 +0900

    [improved]: 소켓 방식 수정

--- deps.ts
+++ deps.ts
@@ -1,4 +1,8 @@
 import 'https://deno.land/x/dotenv/load.ts'
 
-export * as ws from 'https://deno.land/std/ws/mod.ts'
-export { serve } from 'https://deno.land/std/http/mod.ts'
+export { acceptWebSocket, acceptable, isWebSocketCloseEvent } from 'https://deno.land/std@0.103.0/ws/mod.ts'
+export { serve } from 'https://deno.land/std@0.103.0/http/mod.ts'
+export { Application, Router } from 'https://deno.land/x/oak@v8.0.0/mod.ts'
+export { v4 } from 'https://deno.land/std@0.103.0/uuid/mod.ts'
+
+export type { WebSocket } from 'https://deno.land/std@0.103.0/ws/mod.ts'
--- main.ts
+++ main.ts
@@ -1,31 +1,132 @@
-import { serve, ws } from './deps.ts'
+import {
+  Application,
+  Router,
+  acceptWebSocket,
+  acceptable,
+  WebSocket,
+  isWebSocketCloseEvent,
+  v4,
+} from './deps.ts'
 
-const port: number = Number(Deno.env.get('PORT') || 8080)
+const users = new Map<string, WebSocket>();
 
-for await(const request of serve({ port })) {
-  const wsData={
-    conn: request.conn,
-    bufReader: request.r,
-    bufWriter: request.w,
-    headers: request.headers
-  }
-
-  try {
-    const socketIo = await ws.acceptWebSocket(wsData)
-
-    for await (const event of socketIo) {
-      if (ws.isWebSocketCloseEvent(event)) {
-        console.log('소켓이 닫힘..')
-
-        break
-      } else if (typeof event === 'string') {
-        socketIo.send(event)
-      }
-    }
-  } catch(err) {
-    request.respond({
-      status: 400,
-      body: 'Server Error',
-    })
+function broadcast(message: string, senderId?: string): void {
+  if (!message) return;
+  for (const user of users.values()) {
+    user.send(senderId ? `[${senderId}]: ${message}` : message);
   }
 }
+
+export async function chat(ws: WebSocket): Promise<void> {
+  const userId = v4.generate();
+
+  console.log(userId)
+
+  // Register user connection
+  users.set(userId, ws);
+  broadcast(`${userId} is connected`);
+
+  // Wait for new messages
+  for await (const event of ws) {
+    const message = typeof event === "string" ? event : ""
+
+    broadcast(message, userId);
+
+    // Unregister user connection
+    if (!message && isWebSocketCloseEvent(event)) {
+      users.delete(userId);
+      broadcast(`${userId} is disconnected`);
+      break;
+    }
+  }
+}
+
+const port = Number(Deno.env.get('PORT') || 8080)
+const app = new Application()
+const router = new Router()
+
+router.get('/', (context) => {
+  context.response.body = 'Hello World'
+})
+router.get('/ws', async (context) => {
+  // const sock = await context.upgrade()
+
+  // console.log('context')
+  // console.log(sock)
+
+  // if (acceptable(context.request.serverRequest)) {
+  //   const request = context.request.serverRequest
+  //   const socket = await acceptWebSocket({
+  //     conn: request.conn,
+  //     bufReader: request.r,
+  //     bufWriter: request.w,
+  //     headers: request.headers,
+  //   })
+  //
+  //   console.log('socket')
+  //
+  //   await chat(socket)
+  // } else {
+  //   throw new Error('Error when connecting websocket')
+  // }
+})
+
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+console.log(port)
+
+await app.listen({ port })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { serve, ws } from './deps.ts'
+//
+// const port: number = Number(Deno.env.get('PORT') || 8080)
+//
+// for await(const request of serve({ port })) {
+//   const wsData = {
+//     conn: request.conn,
+//     bufReader: request.r,
+//     bufWriter: request.w,
+//     headers: request.headers
+//   }
+//
+//   try {
+//     console.log(request.conn)
+//     console.log(request.headers)
+//
+//     const socketIo = await ws.acceptWebSocket(wsData)
+//
+//     // console.log(socketIo)
+//
+//     for await (const event of socketIo) {
+//       if (ws.isWebSocketCloseEvent(event)) {
+//         console.log('소켓이 닫힘..')
+//
+//         break
+//       } else if (typeof event === 'string') {
+//         socketIo.send(event)
+//       }
+//     }
+//   } catch(err) {
+//     request.respond({
+//       status: 400,
+//       body: 'Server Error',
+//     })
+//   }
+// }
